3b0a046138994af5fe27e9b38f0cfc4a5bf7cc65
Lisa Oppermann <lisa@lisabaut.de>
2019-03-15 12:22:24 +0800

    feat: read data from markdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,50 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
+import Intros from '../components/intros'
+import Main from '../components/main'
+import Separator from '../components/separator'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+import { getAllMarkdownNodes, getIntroContents, getMainContents } from '../utils/content'
+
+const IndexPage = ({ data }) => {
+  const allNodes = getAllMarkdownNodes(data.allMarkdownRemark.edges)
+  const intros = getIntroContents(allNodes)
+  const mainNodes = getMainContents(allNodes)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Intros introNodes={intros} />
+      <Separator />
+      {mainNodes.map(node => (
+        <Main key={node.id} title={node.frontmatter.title} content={node.html} />)
+      )}
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          excerpt
+          html
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            tag
+            order
+          }
+        }
+      }
+    }
+  }
+`
