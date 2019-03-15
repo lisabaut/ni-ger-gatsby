8f722cf9fd4f6e731e24cb5cad15acf39b855e35
Lisa Oppermann <lisa@lisabaut.de>
2019-03-15 12:07:20 +0800

    feat: add config and content

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,14 @@ require('dotenv').config({
   path: `.env.${ activeEnv }`,
 })
 
+const config = require('./src/config')
+
 module.exports = {
   pathPrefix: process.env.PATH_PREFIX,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: config.siteMetadata.title,
+    description: config.siteMetadata.description,
+    author: config.siteMetadata.author,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,10 +20,11 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${ __dirname }/src/images`,
+        name: `src`,
+        path: `${ __dirname }/src/content`,
       },
     },
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
