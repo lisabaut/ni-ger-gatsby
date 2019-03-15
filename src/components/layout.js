f02f60ada2989b9279334aff75158b3f1cebced1
Lisa Oppermann <lisa@lisabaut.de>
2019-03-15 12:23:32 +0800

    refactor: layout adjustments

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,29 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-import styled from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
+import { GlobalStyle, theme } from '../theme/globalStyles'
 
+import Logo from './logo'
 import Header from './header'
-import './layout.css'
+import Footer from './footer'
+import '../css/reset.css'
 
 const Container = styled.div`
-  width: 100%;
-  min-height: 100%;
-  border-top: 47px solid white;
+  margin: 0 auto;
+  max-width: 1024px;
 
   @media (min-width: 700px) {
-    border-left: 47px solid white;
-    border-right: 47px solid white;
+    border-right: 45px solid white;
+    border-left: 45px solid white;
   }
+`
+
+const Inner = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
 `
 
 const Layout = ({ children }) => (
@@ -35,26 +37,21 @@ const Layout = ({ children }) => (
         }
       }
     `}
-    render={data => (
-      <Container>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </Container>
-    )}
+    render={data => {
+      return (
+        <ThemeProvider theme={theme}>
+          <Container>
+            <Logo />
+            <Header siteTitle={data.site.siteMetadata.title} />
+            <Inner>
+              <main>{children}</main>
+              <Footer />
+            </Inner>
+            <GlobalStyle />
+          </Container>
+        </ThemeProvider>
+      )
+    }}
   />
 )
 
